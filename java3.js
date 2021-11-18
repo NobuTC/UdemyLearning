2339c5cfb4be536cc22212f6416522102911c764
NobuTC <jenni90232@gmail.com>
2021-11-18 22:25:25 +0200

    added truth

---
+++ java3.js
@@ -0,0 +1,36 @@
+// const userInput = prompt("Enter something");
+// if (userInput) {
+//   console.log("Truthy!");
+// } else {
+//   console.log("Falsy!");
+// }
+
+// if (0) {
+//   console.log("Truthy!!");
+// } else {
+//   console.log("falsy");
+// }
+
+// if (1942) {
+//   console.log("Truthy!!");
+// } else {
+//   console.log("falsy");
+// }
+
+// if (NaN) {
+//   console.log("Truthy!!");
+// } else {
+//   console.log("falsy");
+// }
+
+if (undefined) {
+  console.log("Truthy!!");
+} else {
+  console.log("falsy");
+}
+
+if (" ") {
+  console.log("Truthy!!");
+} else {
+  console.log("falsy");
+}
