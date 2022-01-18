17d10aba9d8c6d4e5d45f10a5a8e43a8456bf7d5
NobuTC <jenni90232@gmail.com>
2022-01-18 23:21:31 +0200

    new arrayPratice

---
+++ arrayPratice.js
@@ -0,0 +1,11 @@
+const comments = [
+  { username: "Tammy", text: "lololol", votes: 9 },
+  { username: "Fishboy", text: "glub glub", votes: 12387 },
+];
+
+const phone = [
+  {
+    product: "aa",
+    quantility: "PaymentAddress",
+  },
+];
--- java4.js
+++ java4.js
@@ -1,6 +1,53 @@
-const password = prompt("Enter your password");
-if (password.length >= 6 && password.indexOf(" ") === -1) {
-  console.log("Valid password");
-} else {
-  console.log("Incorrect format for password!");
-}
+// const password = prompt("Enter your password");
+// if (password.length >= 6 && password.indexOf(" ") === -1) {
+//   console.log("Valid password");
+// } else {
+//   console.log("Incorrect format for password!");
+// }
+
+// const age = -4564;
+// if ((age >= 0 && age < 5) || age >= 65) {
+//   console.log("Free ticket");
+// } else if (age >= 5 && age < 10) {
+//   console.log("5,95$");
+// } else if (age >= 10 && age < 65) {
+//   console.log("18,95$");
+// } else {
+//   console.log("Invalid age!");
+// }
+
+// let firstName = prompt("Enter your name");
+// if (!firstName) {
+//   firstName = prompt("Plz write yuor name");
+// }
+
+// const age = 2;
+// if (!(age >= 0 && age < 5) || age >= 65) {
+//   console.log("You are not yound or old");
+// }
+
+// const day = 45;
+// switch (day) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("No days");
+// }
