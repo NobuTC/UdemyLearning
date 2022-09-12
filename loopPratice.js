c83652f99fac940e846a6df108dfa11bccc2548b
NobuTC <jenni90232@gmail.com>
2022-09-13 00:13:05 +0300

    StartedLearnLooping

--- loopPratice.js
+++ loopPratice.js
@@ -9,8 +9,52 @@
 //console.log(9);
 //console.log(10);
 
-for (let num = 1; num <= 10; num += 3) {
-  console.log(num);
-}
+//for (let num = 1; num <= 10; num += 3) {
+//  console.log(num);
+//}
 
 //num += 1 TAI num++
+
+/*for (let i = 0; i <= 20; i += 2) {
+  console.log(i);
+}
+// nyt tulee vain joka toinen eli 0,2,4,6...
+*/
+/*
+for (let i = 100; i >= 0; i -= 10) {
+  console.log(i);
+}
+*/
+// nyt tulee 100, 90, 80, 70.... 0 Sillä i>= 0; ja i-=10
+/*
+const names = ["jenni", "truc", "jenna", "vanessa", "susanna", "linh"];
+/*
+for (let i = 0; i < names.length; i++) {
+  console.log(i, names[i]);
+}
+Näin tulee numero ja nimi näkyviin!
+*/
+/*
+for (let i = names.length - 1; i >= 0; i--) {
+  console.log(names[i]);
+}
+// nyt tulee takaperin eli Linh ensin sitten vikana jenni
+*/
+
+/*
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for (let i = 0; i < people.length; i++) {
+  let name = people[i];
+  let result = name.toUpperCase();
+  console.log(result);
+}
+toUpperCase saadaan isot kirjaimet ja annetaan 2 lisää var jotta voidaa 
+Saada nimet näkyviin näin 
+SCOOBY
+VELMA
+DAPHNE
+SHAGGY
+FRED
+*/
