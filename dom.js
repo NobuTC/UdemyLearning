badd652187c3632a193d6048931fb8bd3fcbe0ad
NobuTC <jenni90232@gmail.com>
2022-10-16 21:09:02 +0300

    dom

---
+++ dom.js
@@ -0,0 +1,61 @@
+/*Let's get some practice using getElementById. I've provided a little bit of markup for you (please don't change it!)  Your goal is to write code in app.js to select the following elements using getElementById:
+
+Select the image element by its id and save it to a variable called image
+
+Select the h1 by its id and save it to a variable called heading
+
+Note: You may need to reset the code before attempting this exercise
+*/
+
+/*
+
+        var image = document.getElementById('unicorn')
+       
+        
+     
+        var heading = document.getElementById('mainheading')*/
+
+//      elementtejä
+//        document.getElementsByTagName('')
+//          document.getElementsByclassName('')
+
+/*
+    const allImages = document.getElementsByTagName(img);
+
+    for (let img of allImages) {
+        console.log(img.src)
+    }
+
+    // printtaa kaikki kuvan lähteet..
+    */
+
+/*
+const .. = document.getElementsByClassName('...');
+
+for(let img of squareImages){
+    img.src='hhtps://blahblah.org/uusi/treeniä.jpg'
+}
+
+Tämän jälkeen tulee vain sitä yhtä kuvaa html!!
+jos ei löydy mitään niin ei tuota mitään.
+eli null tai se img
+
+
+*/
+TOKA TEHTÄVÄ!
+//document.querySelector('')
+//ettii esim. # img ja sitten . kuvan
+//myös ('a[title="Java"]')
+// printtaa silloin <a href="/wiki/java" title="Java">Java</a>
+
+
+
+/*I've provided you with some simple markup in index.html.  Your task is to use querySelector and querySelectorAll to select some of those elements.
+
+Select all elements that have the class of "done" and save them in a variable called doneTodos.
+
+Select the one checkbox and save it in a variable called checkbox. Be careful, there is more than one input element on the page! You'll need to select using the type attribute. (if you can't remember the css attribute selector...google it! That's what I would do!)
+
+Write your code in app.js.  Please do not change any of the elements in index.html
+*/
+
